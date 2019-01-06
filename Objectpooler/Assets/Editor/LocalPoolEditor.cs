18f5c5d13f766063c1e02f9e50b0aa9fc65bb200
DANS-PC\Daniel <dmarton94@gmail.com>
2019-01-06 12:58:15 +1100

    - Started to implement the editor window for the Localized Object Pooling script.

---
+++ Objectpooler/Assets/Editor/LocalPoolEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//******************************
+//
+//  Created by: Daniel Marton
+//
+//  Last edited by: Daniel Marton
+//  Last edited on: 6/1/2019
+//
+//******************************
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+[InitializeOnLoad]
+public class LocalPoolEditor : EditorWindow {
+
+    //**************************************************************************************************************
+    //                                                                                                             *
+    //      FUNCTIONS                                                                                              *
+    //                                                                                                             *
+    //**************************************************************************************************************
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    //  Adds a menu item to the Unity Editor top toolbar.
+    //  Brings up the LocalPoolEditor window when clicked on from the toolbar.
+    /// </summary>
+    [MenuItem("Window/Localized Object Pool")]
+    public static void ShowWindow() {
+
+        GetWindow<LocalPoolEditor>("Object Pool");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    //  Static constructor.
+    /// </summary>
+    static LocalPoolEditor() {
+        
+        Debug.Log("PRELOADING OBJECTS");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    //  
+    /// </summary>
+    public void OnGUI() {
+
+        EditorGUILayout.Space();
+
+        // Header/title
+        EditorGUILayout.BeginVertical();
+        GUILayout.Label("LOCALIZED OBJECT POOL DEBUGGER", EditorStyles.boldLabel);
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.Space();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
